3f25b5d7f411442966cf0d1e6a203c63cb6cf582
Sam Potter <sam_pot_97@hotmail.com>
2016-11-22 20:15:01 +0000

    Initial project creation

---
+++ Source/SquaredOff/Public/SQBaseGamemode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SQBaseGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUAREDOFF_API ASQBaseGamemode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
