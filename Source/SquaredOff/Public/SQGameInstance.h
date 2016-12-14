1dba1a85b4304fe7251b0b7e789a7d9d1afa8ce8
Sam Potter <sam_pot_97@hotmail.com>
2016-12-14 22:15:29 +0000

    Splash Screen fixes
    Fade in out durations separate
    Won't replay splashscreen everytime player re-enters main menu
    Added new font

---
+++ Source/SquaredOff/Public/SQGameInstance.h
@@ -0,0 +1,19 @@
+// SquaredOff - TODO: Write notice
+
+#pragma once
+
+#include "Engine/GameInstance.h"
+#include "SQGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUAREDOFF_API USQGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
